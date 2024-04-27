b01271daba42f1d616932aee103445a27c42bd57
Ahmet Kasap <aahmetkasap@gmail.com>
2024-04-27 15:04:13 +0300

    start

---
+++ src/main/kotlin/com/piksel/RestfullApiApplication.kt
@@ -0,0 +1,11 @@
+package com.piksel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestfullApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestfullApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/piksel/RestfullApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.piksel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestfullApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
